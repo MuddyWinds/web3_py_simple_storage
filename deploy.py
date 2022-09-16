2544605bd16c26bb6cd50e8cf280b6c334516c65
MuddyWinds <muddywinds@gmail.com>
2022-09-17 02:54:58 +0800

    Edited deploy.py to test the deploying function of the smart contract

--- deploy.py
+++ deploy.py
@@ -1,4 +1,7 @@
+import json
+from web3 import Web3
 from solcx import compile_standard, install_solc
+from private_keys import private_key_01
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -20,4 +23,36 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0",
 )
-print(compiled_sol)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# Get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# Get ABI
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# For connecting to Ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x40D4C2F5e13B299a71df365511C63c3E5151819e"
+private_key = private_key_01
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# Build -> Sign -> Send a transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
